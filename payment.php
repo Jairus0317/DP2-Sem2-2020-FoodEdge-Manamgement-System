e17275ca396017b4c2211f142c8650ccaa7e0734
kibboby <nrozunan@gmail.com>
2020-10-14 20:32:29 +0800

    chg folder

--- payment.php
+++ payment.php
@@ -14,7 +14,7 @@ if (isset($_SESSION['user'])) {
 
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style/style_A.css">
     <script language="JavaScript" type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script language="JavaScript" type="text/javascript" src="jQuery.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
