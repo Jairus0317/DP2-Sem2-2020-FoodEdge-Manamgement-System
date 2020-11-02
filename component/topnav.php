44eacd0642623a60bed9a743b616beaab7559e15
Jairus Yong <jairus0317@gmail.com>
2020-11-02 19:18:06 +0800

    Fix header (navbar), create mangement team page

---
+++ component/topnav.php
@@ -0,0 +1,24 @@
+<header>
+	<nav class="navbar navbar-expand-sm navbar-dark justify-content-between">
+		<ul class="navbar-nav">
+			<a class="navbar-brand" href="#">
+   			 <img src="./images/logo2.png" alt="Logo" width="150px">
+  			</a>
+			<li class="nav-item">
+			<a class="nav-link" href="AboutPage.php">About Us</a>
+			</li>
+			<li class="nav-item">
+			<a class="nav-link" href="CateringServices.php">Catering Services</a>
+			</li>
+			<li class="nav-item">
+			<a class="nav-link" href="contactus.php">Contact Us</a>
+			</li>
+			<li class="nav-item">
+			<a class="nav-link" href="feedback.php">Feedback</a>
+			</li>
+		</ul>
+
+        <a href="login.php" class="btn btn-primary"><i class="fa fa-sign-in"></i> Login</a>
+
+	</nav>
+</header>
---
+++ mangementteam.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="Cache-control" content="no-cache">
+	<!--Bootstrap CDN-->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+	<!--FontAwesome CDN-->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+	<link rel="stylesheet" href="style/style.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+	<title>Buffet Menu</title>
+</head>
+
+<body>
+	<?php require_once ("component/topnav.php"); ?>
+
+	<div class = "container-fluid my-container BuffetMenuPage">
+
+	</div>
+
+</body>
+
+</html>
